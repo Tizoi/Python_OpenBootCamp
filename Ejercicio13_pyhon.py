c0376ab54ca3ba670bc805bd94bb4735c5f95487
Abel <abelboy94@gmail.com>
2022-08-16 13:27:07 +0200

    Create Ejercicio13_pyhon.py

---
+++ Ejercicio13_pyhon.py
@@ -0,0 +1,15 @@
+class vehiculo():
+    _color = "Negro"
+    _ruedas = 4
+    _puertas = 4
+
+class coche(vehiculo):
+    _velocidad = 120
+    _cilindrada = "125cc"
+
+
+
+
+miCoche = coche()
+
+print(miCoche)
