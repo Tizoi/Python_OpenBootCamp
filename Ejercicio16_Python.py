86684a7209d2d73c3c545570277e613c2085ddb0
Abel <abelboy94@gmail.com>
2022-08-16 16:49:02 +0200

    Create Ejercicio16_Python.py

---
+++ Ejercicio16_Python.py
@@ -0,0 +1,17 @@
+import time
+
+def main():
+
+    print('¿Es hora de ir a casa?')
+
+    hora = time.strftime('%H')
+    minutos = time.strftime('%M')
+
+    if int(hora) >= 19:
+        print ("Es hora de irse a casa")
+    else:
+        print ("Quedan {} horas y {} minutos para irnos a casa".format(18- int(hora), 59-int(minutos)))
+
+
+if __name__ == '__main__':
+    main()
